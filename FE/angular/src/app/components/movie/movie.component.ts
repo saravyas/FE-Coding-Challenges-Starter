d79c51fae47f89b1b0be3272e3227b982e9dbb77
saravyas <saravyas007@gmail.com>
2023-09-26 09:49:29 +0530

    :bug: Movie Detail page fix
    movie detail page load issue fixed

--- FE/angular/src/app/components/movie/movie.component.ts
+++ FE/angular/src/app/components/movie/movie.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { tap } from 'rxjs';
+import { Subscription, tap } from 'rxjs';
 import { DataService, MovieComplete } from '../../services/data.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html'
 })
 export class MovieComponent implements OnDestroy, OnInit {
-  public movie: MovieComplete;
-  public movieId = '';
-  private movieSubscription: any;
+  public movie: MovieComplete | undefined;
+  movieId: string | null = '';
+  private movieSubscription: Subscription;
+  private destroy$ = new Subject<void>();
 
   constructor(private activatedRoute: ActivatedRoute, private dataService: DataService) {}
 
   public ngOnInit() {
-    this.activatedRoute.params.pipe(tap(({ id }) => (this.movieId = id)));
-    this.movieSubscription = this.dataService.getMovie(this.movieId).pipe(tap((data) => (this.movie = data)));
+    this.movieId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.movieSubscription = this.dataService
+      .getMovies()
+      .pipe(
+        takeUntil(this.destroy$),
+        tap((movies) => {
+          this.movie = movies.Search.find((movie) => movie.imdbID === this.movieId);
+        })
+      )
+      .subscribe();
   }
 
   public ngOnDestroy(): void {
     this.movieSubscription.unsubscribe();
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
